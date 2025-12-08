208bc28352b06bd37ec0c1d0d6f840d635e45383
Susan O <110050655+QueenMe1@users.noreply.github.com>
2025-12-07 22:59:01 -0500

    updates

---
+++ project/scripts/index_script.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('.nav');
+const menuButton = document.getElementById('.nav-bar');
+
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('active')
+});
